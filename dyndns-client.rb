4b0d2f06d5669a09d798300d17cd78ca114a92f2
Josef Stautner <hello@veloc1ty.de>
2019-02-06 17:43:14 +0100

    Version bump to 1.0.1

--- dyndns-client.rb
+++ dyndns-client.rb
@@ -1,9 +1,9 @@
 class DyndnsClient < Formula
   desc "Official dyndns.host client"
   homepage "https://dyndns.host"
-  url "https://dyndns.host/clients/dyndns-client-darwin-amd64-v1.0.0.tar.gz"
-  sha256 "0d1344cc6db39ac171fb97e08f14ae49c2d365bc49460725489e804d4b74456b"
-  version "1.0.0"
+  url "https://dyndns.host/clients/dyndns-client-darwin-amd64-1.0.1.tar.gz"
+  sha256 "084179b260f8515a07a690246293f444c900bc407c9bd5a7b7cfc6c05fe7114b"
+  version "1.0.1"
 
   def install
     bin.install "dyndns-client"
