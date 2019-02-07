ac84fcbf96c367d27d979eb325323525e96f392b
Josef Stautner <hello@veloc1ty.de>
2019-02-07 19:12:05 +0100

    changed dl url

--- dyndns-client.rb
+++ dyndns-client.rb
@@ -1,7 +1,7 @@
 class DyndnsClient < Formula
   desc "Official dyndns.host client"
   homepage "https://dyndns.host"
-  url "https://dyndns.host/clients/dyndns-client-darwin-amd64-1.0.1.tar.gz"
+  url "https://mirror.dyndns.host/clients/dyndns-client-darwin-amd64-1.0.1.tar.gz"
   sha256 "084179b260f8515a07a690246293f444c900bc407c9bd5a7b7cfc6c05fe7114b"
   version "1.0.1"
 
